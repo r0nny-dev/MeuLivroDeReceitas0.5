3cee78e3a4ad9944a1b9797af5377300a07e349c
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-07-11 13:39:49 -0300

    Create Migration/Database.cs

---
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Migrations/Database.cs
@@ -0,0 +1,23 @@
+using Dapper;
+using Npgsql;
+
+namespace MeuLivroDeReceitas.Infrastructure.Migrations;
+
+public static class Database
+{
+
+    public static void CriarDatabase(string connectionString, string nomeDatabase)
+    {
+        using var conn = new NpgsqlConnection(connectionString);
+
+        var parametros = new DynamicParameters();
+        parametros.Add("nome", nomeDatabase);
+
+        var registros = conn.Query("SELECT * FROM pg_database WHERE datname = @nome", parametros);
+
+        if (!registros.Any())
+        {
+            conn.Execute($"CREATE DATABASE {nomeDatabase}");
+        }
+    }
+}
