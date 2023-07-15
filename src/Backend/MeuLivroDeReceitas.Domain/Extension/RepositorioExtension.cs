358ebbf5e497d994c5c4f4e42604593a2b291dea
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-07-15 10:31:54 -0300

    Update RepositorioExtension.cs

--- src/Backend/MeuLivroDeReceitas.Domain/Extension/RepositorioExtension.cs
+++ src/Backend/MeuLivroDeReceitas.Domain/Extension/RepositorioExtension.cs
@@ -5,16 +5,8 @@ namespace MeuLivroDeReceitas.Domain.Extension;
 public static class RepositorioExtension
 {
     public static string GetNomeDatabase(this IConfiguration configurationManager)
-    {
-        var nomeDatabase = configurationManager.GetConnectionString("NomeDatabase");
-
-        return nomeDatabase;
-    }
+        => configurationManager.GetConnectionString("NomeDatabase");
 
     public static string GetConexaoDatabase(this IConfiguration configurationManager)
-    {
-        var connectionString = configurationManager.GetConnectionString("Conexao");
-
-        return connectionString;
-    }
+        => configurationManager.GetConnectionString("Conexao");
 }
